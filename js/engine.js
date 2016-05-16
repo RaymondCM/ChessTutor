96a96d82a633e112c936a4886d4704944083ae94
Luke Jackson <jacksonluke96@yahoo.co.uk>
2016-05-16 21:32:08 +0100

    fps

--- js/engine.js
+++ js/engine.js
@@ -11,6 +11,7 @@ function Init_Stockfish() {
 //Query the engine from the tutor or opponent
 function AskEngine(source, side) {
     var query = {
+<<<<<<< Updated upstream
         source: source,
         side: side,
         move: 'undefined'
@@ -20,6 +21,23 @@ function AskEngine(source, side) {
 
 //Message recieved
 engine.onmessage = function (event) {
+=======
+                source: source,
+                side: side,
+                move: 'undefined'
+            };
+    queue.push(query);  
+    if (queue.length === 1)
+        {
+            console.log('queuelength = 0');
+            engine.postMessage("position startpos");
+            engine.postMessage("go depth 15");
+        }
+}
+
+//Message recieved
+engine.onmessage = function(event) {
+>>>>>>> Stashed changes
     console.log(event.data);
     //When the engine outputs 'bestmove' the search has finished
     if (String(event.data).substring(0, 8) == 'bestmove') {
