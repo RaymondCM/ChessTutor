480a5bfbd1757d25a4f9f7113826da276113f9fd
Luke Jackson <jacksonluke96@yahoo.co.uk>
2016-05-16 20:30:56 +0100

    Queue created

--- engine.js
+++ engine.js
@@ -1,16 +1,27 @@
 function Init_Stockfish() {
-    requestQueue = [];
+    queue = [];
+    
     stockfish = new Worker('stockfish.js');
+    
+    
+    AskEngine('opp', 'white');
 }
 
 //Query the engine from the tutor or opponent
 function AskEngine(var source, side){
-    var query = {source: source,
-                 side: side};
+    var query = {
+                source: source,
+                side: side,
+                move: 'undefined'
+            };
     requestQueue.push(query);
 };
 
 //Message recieved
 engine.onmessage = function(event) {
   console.log(event.data);
+    //When the engine outputs 'bestmove' the search has finished
+        if (String(event.data).substring(0, 8) == 'bestmove') {
+            //Format the results
+            console.log(event.data);
 };
