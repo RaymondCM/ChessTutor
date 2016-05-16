9bb4651faa459a95b7812b0208c55e6536b2a448
Luke Jackson <jacksonluke96@yahoo.co.uk>
2016-05-16 21:36:57 +0100

    again

--- js/engine.js
+++ js/engine.js
@@ -14,27 +14,19 @@ function AskEngine(source, side) {
         source: source,
         side: side,
         move: 'undefined'
-    };
+    }
+    
     requestQueue.push(query);
+    if (queue.length === 1) {
+        console.log('queuelength = 0');
+        engine.postMessage("position startpos");
+        engine.postMessage("go depth 15");
+    }  
 }
 
 //Message recieved
 engine.onmessage = function (event) {
-                source: source,
-                side: side,
-                move: 'undefined'
-            };
-    queue.push(query);  
-    if (queue.length === 1)
-        {
-            console.log('queuelength = 0');
-            engine.postMessage("position startpos");
-            engine.postMessage("go depth 15");
-        }
-}
 
-//Message recieved
-engine.onmessage = function(event) {
     console.log(event.data);
     //When the engine outputs 'bestmove' the search has finished
     if (String(event.data).substring(0, 8) == 'bestmove') {
