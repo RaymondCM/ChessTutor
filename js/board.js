0c0eb487da2098c9230490a2f0d1318311735fe1
Raymond Kirk <ray.tunstill@live.co.uk>
2016-05-17 19:23:04 +0100

    Improved Board

--- js/board.js
+++ js/board.js
@@ -1,5 +1,7 @@
 function Init_Chessboard() {
 
+    SetTheme(themes[0]);
+
     if (!ct_debug)
         $('#Debug').css('display', 'none');
 
@@ -115,4 +117,35 @@ function Init_Chessboard() {
     board = ChessBoard('Chessboard', cfg);
 
     updateStatus();
+}
+
+function SetTheme(theme) {
+
+    $(".whiteSquare").removeClass(".whiteSquare");
+
+
+
+    //changeCss('.white-1e1d7', "background-color: " + cb_whiteSqColour + ";" + " color:" + cb_whiteSqTextColour + ";");
+
+    //changeCss('.black-3c85d', "background-color: " + cb_blackSqColour + ";" + " color:" + cb_blackSqTextColour + ";");
+}
+
+function changeCss(className, classValue) {
+    // we need invisible container to store additional css definitions
+    var cssMainContainer = $('#css-modifier-container');
+    if (cssMainContainer.length == 0) {
+        var cssMainContainer = $('<div id="css-modifier-container"></div>');
+        cssMainContainer.hide();
+        cssMainContainer.appendTo($('body'));
+    }
+
+    // and we need one div for each class
+    classContainer = cssMainContainer.find('div[data-class="' + className + '"]');
+    if (classContainer.length == 0) {
+        classContainer = $('<div data-class="' + className + '"></div>');
+        classContainer.appendTo(cssMainContainer);
+    }
+
+    // append additional style
+    classContainer.html('<style>' + className + ' {' + classValue + '}</style>');
 }
--- js/start.js
+++ js/start.js
@@ -3,9 +3,17 @@ window.onload = function () {
         GLOBAL VARIABLES
     */
     ct_debug = true;
+
+    themes = [{
+        name: "default",
+        whiteSquare: "white",
+        whiteSquareText: "black",
+        blackSquare: "black",
+        blackSquareText: "white"
+    }];
+
     cb_possiblePlacesColourWhiteSq = "blue";
     cb_possiblePlacesColourBlackSq = "red";
-
 
     Init_Chessboard();
     Init_Stockfish();
