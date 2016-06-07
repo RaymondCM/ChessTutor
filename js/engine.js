200c7b724ddcbca7baa421807ad2d382d9f85fff
Raymond Kirk <ray.tunstill@live.co.uk>
2016-06-07 21:25:58 +0100

    Fixed Query Engine

--- js/engine.js
+++ js/engine.js
@@ -15,81 +15,81 @@ function Init_Stockfish() {
 
 //Query the engine from the tutor or opponent
 function AskEngine(source, side, fen, turnCount) {
-    var query = {
-        turnCount: turnCount,
-        source: source,
-        side: side,
-        fen: fen,
-        move: 'QUERY UNRESOLVED'
-    }
-    console.log('pushing');
-    console.log(query);
-    queryQueue.push(query);
-    //If first item, send the query straight away
-    if (queryQueue.length === 1)
-        QueryEngine(query["fen"], sf_searchDepth);
-    else //Stop the engine search
-    {
-        Engine.postMessage("stop");
-        queryQueue[0]['move'] = 'STOP';
-    }
-    console.log("Queue length: " + queryQueue.length);
+	var query = {
+		turnCount: turnCount,
+		source: source,
+		side: side,
+		fen: fen,
+		move: 'QUERY UNRESOLVED'
+	}
+	console.log('pushing');
+	console.log(query);
+	queryQueue.push(query);
+	//If first item, send the query straight away
+	if (queryQueue.length === 1)
+		QueryEngine(query["fen"], sf_searchDepth);
+	else //Stop the engine search
+	{
+		Engine.postMessage("stop");
+		queryQueue[0]['move'] = 'STOP';
+	}
+	console.log("Queue length: " + queryQueue.length);
 }
 
 //Message recieved
 Engine.onmessage = function (event) {
-        //When the engine outputs 'bestmove' the search has finished
-        if ((String(event.data).substring(0, 8) == 'bestmove') && queryQueue[0]) {
-            console.log('BEST MOVE');
-            //If no interruption, assign the move info
-            if (queryQueue[0].move !== 'STOP')
-                queryQueue[0].move = String(event.data).substring(9, 13);
-            //Remove the head of the queue
-            ReturnQuery(queryQueue.shift());
-            //If the queue still has queries, go to next query
-            if (queryQueue.length > 0) {
-                QueryEngine(queryQueue[0]["fen"], sf_searchDepth);
-            }
-        }
-    }
-    //Format string for engine message
+		//When the engine outputs 'bestmove' the search has finished
+		if ((String(event.data).substring(0, 8) == 'bestmove') && queryQueue[0]) {
+			console.log('BEST MOVE');
+			//If no interruption, assign the move info
+			if (queryQueue[0].move !== 'STOP')
+				queryQueue[0].move = String(event.data).substring(9, 13);
+			//Remove the head of the queue
+			ReturnQuery(queryQueue.shift());
+			//If the queue still has queries, go to next query
+			if (queryQueue.length > 0) {
+				QueryEngine(queryQueue[0]["fen"], sf_searchDepth);
+			}
+		}
+	}
+	//Format string for engine message
 function QueryEngine(fen, depth) {
-    Engine.postMessage("position fen " + fen);
-    Engine.postMessage("go depth " + depth);
+	Engine.postMessage("position fen " + fen);
+	Engine.postMessage("go depth " + depth);
 }
 
 function MovePiece(from, to) {
-    var boardPosition = board.position();
-    
-    //console.log('MOVING PIECE FROM AI');
-    game.move({
-        from: from,
-        to: to,
-        promotion: 'q'
-    });
-    
-    checkForTaken(boardPosition, to);
+	var boardPosition = board.position();
 
-    board.position(game.fen());
-    updateDebugLog();
+	//console.log('MOVING PIECE FROM AI');
+	game.move({
+		from: from,
+		to: to,
+		promotion: 'q'
+	});
+
+	checkForTaken(boardPosition, to);
+
+	board.position(game.fen());
+	updateDebugLog();
+	updateStatus();
+
 }
 
 function ReturnQuery(query) {
-    $("#suggestedMove").html("SUGGESTED MOVE FOR " + (query['side'] == "w" ? "WHITE" : "BLACK") + ": " + query['move']);
-    
-    console.log("Turn: " + query['turnCount'] + " Side: " + query['side'] + " Move: " + query['move']);
+	$("#suggestedMove").html("SUGGESTED MOVE FOR " + (query['side'] == "w" ? "WHITE" : "BLACK") + ": " + query['move']);
 
-    if (game.game_over()) return;
-    
-    //Make opponent moves
-    if (cb_autoPlay || (game_pve && (game_playerSide != query.side)))
-        {
-            console.log('MOVING PIECE FROM AI');
-            //var makeMove = function () MovePiece(query.move.substr(0, 2), query.move.substr(2, 4));
-            setTimeout(MovePiece, cb_autoPlayDelay, query.move.substr(0, 2), query.move.substr(2, 4));
-            updateStatus();
-            //AskEngine('INSERT SOURCE', game.turn(), game.fen(), Math.floor(turnCount / 2));
-        }
-    
+	console.log("Turn: " + query['turnCount'] + " Side: " + query['side'] + " Move: " + query['move']);
+
+	if (game.game_over()) return;
+
+	//Make opponent moves
+	if (cb_autoPlay || (game_pve && (game_playerSide != query.side))) {
+		console.log('MOVING PIECE FROM AI');
+		//var makeMove = function () MovePiece(query.move.substr(0, 2), query.move.substr(2, 4));
+		setTimeout(MovePiece, cb_autoPlayDelay, query.move.substr(0, 2), query.move.substr(2, 4));
+		//AskEngine('INSERT SOURCE', game.turn(), game.fen(), Math.floor(turnCount / 2));
+	}
+
 
 }
--- js/start.js
+++ js/start.js
@@ -1,49 +1,49 @@
 window.onload = function () {
-    /*
-        GLOBAL VARIABLES
-    */
-    ct_debug = true;
+	/*
+	    GLOBAL VARIABLES
+	*/
+	ct_debug = true;
 
-    cb_shapes = {
-        Square: "border-radius: 0px;",
-        RoundedSquare: "border-radius: 10px;",
-        Circle: "border-radius: 50px;",
-        Skew: "-webkit-transform: rotate(-45deg); - moz - transform: rotate(-45 deg); - ms - transform: rotate(-45 deg); - o - transform: rotate(-45 deg);transform: rotate(-45 deg); - webkit - transform - origin: 0 100 % ; - moz - transform - origin: 0 100 % ; - ms - transform - origin: 0 100 % ; - o - transform - origin: 0 100 % ;transform - origin: 0 100 % ;",
-        Diamond: "-webkit-transform: rotate(-45deg); - moz - transform: rotate(-45 deg); - ms - transform: rotate(-45 deg); - o - transform: rotate(-45 deg);transform: rotate(-45 deg); - webkit - transform - origin: 0 100 % ; - moz - transform - origin: 0 100 % ; - ms - transform - origin: 0 100 % ; - o - transform - origin: 0 100 % ;transform - origin: 0 100 % ;",
-        DiamondIMGFix: "-webkit-transform: rotate(45deg); - moz - transform: rotate(45 deg); - ms - transform: rotate(45 deg); - o - transform: rotate(45 deg);transform: rotate(45 deg); - webkit - transform - origin: 0 100 % ; - moz - transform - origin: 0 100 % ; - ms - transform - origin: 0 100 % ; - o - transform - origin: 0 100 % ;transform - origin: 0 100 % ;"
-    };
+	cb_shapes = {
+		Square: "border-radius: 0px;",
+		RoundedSquare: "border-radius: 10px;",
+		Circle: "border-radius: 50px;",
+		Skew: "-webkit-transform: rotate(-45deg); - moz - transform: rotate(-45 deg); - ms - transform: rotate(-45 deg); - o - transform: rotate(-45 deg);transform: rotate(-45 deg); - webkit - transform - origin: 0 100 % ; - moz - transform - origin: 0 100 % ; - ms - transform - origin: 0 100 % ; - o - transform - origin: 0 100 % ;transform - origin: 0 100 % ;",
+		Diamond: "-webkit-transform: rotate(-45deg); - moz - transform: rotate(-45 deg); - ms - transform: rotate(-45 deg); - o - transform: rotate(-45 deg);transform: rotate(-45 deg); - webkit - transform - origin: 0 100 % ; - moz - transform - origin: 0 100 % ; - ms - transform - origin: 0 100 % ; - o - transform - origin: 0 100 % ;transform - origin: 0 100 % ;",
+		DiamondIMGFix: "-webkit-transform: rotate(45deg); - moz - transform: rotate(45 deg); - ms - transform: rotate(45 deg); - o - transform: rotate(45 deg);transform: rotate(45 deg); - webkit - transform - origin: 0 100 % ; - moz - transform - origin: 0 100 % ; - ms - transform - origin: 0 100 % ; - o - transform - origin: 0 100 % ;transform - origin: 0 100 % ;"
+	};
 
-    //Eventually enable this obj to be loaded from a file on the server (and/or user account)
-    var cb_themes = [{
-        name: "default",
-        whiteSquare: "#ffeccc",
-        whiteSquareText: "#8d5b36",
-        blackSquare: "#8d5b36",
-        blackSquareText: "#ffeccc",
-        whitePossiblePlaces: "rgba(210, 211, 210, 0.05)",
-        blackPossiblePlaces: "rgba(210, 211, 210, 0.15)",
-        boardShape: "Square"
+	//Eventually enable this obj to be loaded from a file on the server (and/or user account)
+	var cb_themes = [{
+		name: "default",
+		whiteSquare: "#ffeccc",
+		whiteSquareText: "#8d5b36",
+		blackSquare: "#8d5b36",
+		blackSquareText: "#ffeccc",
+		whitePossiblePlaces: "rgba(210, 211, 210, 0.05)",
+		blackPossiblePlaces: "rgba(210, 211, 210, 0.15)",
+		boardShape: "Square"
     }, {
-        name: "whacky",
-        whiteSquare: "#ffccf5",
-        whiteSquareText: "##00f7f7",
-        blackSquare: "#00f7f7",
-        blackSquareText: "#ffccf5",
-        whitePossiblePlaces: "rgba(245, 255, 0, 0.55)",
-        blackPossiblePlaces: "rgba(245, 255, 0, 0.55)",
-        boardShape: "Skew"
+		name: "whacky",
+		whiteSquare: "#ffccf5",
+		whiteSquareText: "##00f7f7",
+		blackSquare: "#00f7f7",
+		blackSquareText: "#ffccf5",
+		whitePossiblePlaces: "rgba(245, 255, 0, 0.55)",
+		blackPossiblePlaces: "rgba(245, 255, 0, 0.55)",
+		boardShape: "Skew"
     }];
 
-    game_pve = true;
-    game_playerSide = 'b';
-    
-    cb_currentTheme = cb_themes[0];
-    cb_autoPlay = true;
-    cb_autoPlayDelay = 100;
-    cb_fenHistoryMaxLength = 10;
-    
-    sf_searchDepth = '15';
+	game_pve = true;
+	game_playerSide = 'b';
 
-    Init_Chessboard();
-    Init_Stockfish();
+	cb_currentTheme = cb_themes[0];
+	cb_autoPlay = false;
+	cb_autoPlayDelay = 3000;
+	cb_fenHistoryMaxLength = 10;
+
+	sf_searchDepth = '6';
+
+	Init_Chessboard();
+	Init_Stockfish();
 }
