1f54e2b9087976935261450289825b549440216d
Raymond Kirk <ray.tunstill@live.co.uk>
2016-06-12 10:07:56 +0100

    Decreased ambiguity

--- js/board.js
+++ js/board.js
@@ -1,148 +1,148 @@
 function Init_Chessboard() {
 
-    turnCount = 0;
-    fenHistory = [];
+	turnCount = 0;
+	fenHistory = [];
 
-    SetTheme(cb_currentTheme);
+	SetTheme(cb_currentTheme);
 
-    if (!ct_debug)
-        $('#Debug').css('display', 'none');
+	if (!ct_debug)
+		$('#Debug').css('display', 'none');
 
-    board = "";
-    game = new Chess();
+	board = "";
+	game = new Chess();
 
-    //Piece Map
-    pieces = {
-        K: "King"
-        , N: "Knight"
-        , P: "Pawn"
-        , B: "Bishop"
-        , Q: "Queen"
-        , R: "Rook"
-    };
+	//Piece Map
+	pieces = {
+		K: "King",
+		N: "Knight",
+		P: "Pawn",
+		B: "Bishop",
+		Q: "Queen",
+		R: "Rook"
+	};
 
-    // do not pick up pieces if the game is over
-    // only pick up pieces for the side to move
-    var onDragStart = function (source, piece, position, orientation) {
-        if (game.game_over() === true ||
-            (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-            (game.turn() === 'b' && piece.search(/^w/) !== -1) ||
+	// do not pick up pieces if the game is over
+	// only pick up pieces for the side to move
+	var onDragStart = function (source, piece, position, orientation) {
+		if (game.game_over() === true ||
+			(game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+			(game.turn() === 'b' && piece.search(/^w/) !== -1) ||
 
-            (game.turn() !== game_playerSide)) {
-            return false;
-        }
-    };
+			(game.turn() !== game_playerSide)) {
+			return false;
+		}
+	};
 
-    MovePiece = function (from, to) {
-        
-        
-        var move = game.move({
-            from: from
-            , to: to
-            , promotion: 'q'
-        });
-        
-        if (move === null) return 'snapback';
-        
-        checkForTaken(board.position());
-        board.position(game.fen());
-        turnCount++;
-        
-        updateStatus();
-    }
-    updateStatus = function () {
-        checkForTaken(board.position());
-        updateDebugLog();
-        
+	MovePiece = function (from, to) {
 
 
-        if (fenHistory.lengh <= cb_fenHistoryMaxLength)
-            fenHistory.push(game.fen());
-        else {
-            fenHistory.shift();
-            fenHistory.push(game.fen());
-        }
-        
-        AskEngine('INSERT SOURCE', game.turn(), game.fen());
-    };
+		var move = game.move({
+			from: from,
+			to: to,
+			promotion: 'q'
+		});
 
-    var removeHighlighting = function () {
-        $('#Chessboard .square-55d63').css('background', '');
-        board.highlightSquare(cb_permHighlighted, true);
-    };
+		if (move === null) return 'snapback';
 
-    highlightSquare = function (square, aiHighlight) {
-        var colourWhite, colourBlack;
+		checkForTaken(board.position());
+		board.position(game.fen());
+		turnCount++;
 
-        if (!aiHighlight) {
-            colourWhite = cb_currentTheme.whitePossiblePlaces;
-            colourBlack = cb_currentTheme.blackPossiblePlaces;
-        } else {
-            colourWhite = cb_currentTheme.permWhitePossiblePlaces;
-            colourBlack = cb_currentTheme.permBlackPossiblePlaces;
-        }
+		updateStatus();
+	}
+	updateStatus = function () {
+		checkForTaken(board.position());
+		updateDebugLog();
 
-        var squareEl = $('#Chessboard .square-' + square);
 
-        var background = squareEl.hasClass('black-3c85d') === true ? colourBlack : colourWhite;
 
-        squareEl.css('background', background);
-    };
+		if (fenHistory.lengh <= cb_fenHistoryMaxLength)
+			fenHistory.push(game.fen());
+		else {
+			fenHistory.shift();
+			fenHistory.push(game.fen());
+		}
 
-    var onMouseoverSquare = function (square, piece) {
-        // get list of possible moves for this square
-        var moves = game.moves({
-            square: square
-            , verbose: true
-        });
+		AskstockfishEngine('INSERT SOURCE', game.turn(), game.fen());
+	};
 
-        var regex = new RegExp("/^" + game_playerSide + "/");
-        if (game.turn() !== game_playerSide) {
-            return;
-        }
+	var removeHighlighting = function () {
+		$('#Chessboard .square-55d63').css('background', '');
+		board.highlightSquare(cb_permHighlighted, true);
+	};
 
-        // exit if there are no moves available for this square
-        if (moves.length === 0) return;
+	highlightSquare = function (square, aiHighlight) {
+		var colourWhite, colourBlack;
 
-        // highlight the square they moused over
-        highlightSquare(square, false);
+		if (!aiHighlight) {
+			colourWhite = cb_currentTheme.whitePossiblePlaces;
+			colourBlack = cb_currentTheme.blackPossiblePlaces;
+		} else {
+			colourWhite = cb_currentTheme.permWhitePossiblePlaces;
+			colourBlack = cb_currentTheme.permBlackPossiblePlaces;
+		}
 
-        // highlight the possible squares for this piece
-        for (var i = 0; i < moves.length; i++) {
-            highlightSquare(moves[i].to, false);
-        }
-    };
+		var squareEl = $('#Chessboard .square-' + square);
 
-    var onMouseoutSquare = function (square, piece) {
-        removeHighlighting();
-    };
+		var background = squareEl.hasClass('black-3c85d') === true ? colourBlack : colourWhite;
 
-    var onSnapEnd = function () {
-        board.position(game.fen());
-    };
+		squareEl.css('background', background);
+	};
 
-    var cfg = {
-        draggable: true
-        , position: 'start'
-        , onDragStart: onDragStart
-        , onDrop: MovePiece
-        , onMouseoutSquare: onMouseoutSquare
-        , onMouseoverSquare: onMouseoverSquare
-        , onSnapEnd: onSnapEnd
-    };
+	var onMouseoverSquare = function (square, piece) {
+		// get list of possible moves for this square
+		var moves = game.moves({
+			square: square,
+			verbose: true
+		});
 
-    board = ChessBoard('Chessboard', cfg);
-    board.orientation((game_playerSide === 'w') ? 'white' : 'black');
-    
-    turnCount++;
-    updateStatus();
-    board.highlightSquare = function (s, b) {
-        for (x in s) {
-            highlightSquare(s[x], b);
-        }
-    };
+		var regex = new RegExp("/^" + game_playerSide + "/");
+		if (game.turn() !== game_playerSide) {
+			return;
+		}
 
-    board.highlightSquare(cb_permHighlighted, true);
+		// exit if there are no moves available for this square
+		if (moves.length === 0) return;
+
+		// highlight the square they moused over
+		highlightSquare(square, false);
+
+		// highlight the possible squares for this piece
+		for (var i = 0; i < moves.length; i++) {
+			highlightSquare(moves[i].to, false);
+		}
+	};
+
+	var onMouseoutSquare = function (square, piece) {
+		removeHighlighting();
+	};
+
+	var onSnapEnd = function () {
+		board.position(game.fen());
+	};
+
+	var cfg = {
+		draggable: true,
+		position: 'start',
+		onDragStart: onDragStart,
+		onDrop: MovePiece,
+		onMouseoutSquare: onMouseoutSquare,
+		onMouseoverSquare: onMouseoverSquare,
+		onSnapEnd: onSnapEnd
+	};
+
+	board = ChessBoard('Chessboard', cfg);
+	board.orientation((game_playerSide === 'w') ? 'white' : 'black');
+
+	turnCount++;
+	updateStatus();
+	board.highlightSquare = function (s, b) {
+		for (x in s) {
+			highlightSquare(s[x], b);
+		}
+	};
+
+	board.highlightSquare(cb_permHighlighted, true);
 
 
 }
@@ -150,178 +150,178 @@ function Init_Chessboard() {
 
 
 function piece(code) {
-    this.colourLetter = code.substr(0, 1);
-    this.nameletter = code.substr(1, 2);
-    this.colorWord = code.substr(0, 1) == 'w' ? 'White' : 'Black';
-    this.nameWord = pieces[code.substr(1, 2)];
+	this.colourLetter = code.substr(0, 1);
+	this.nameletter = code.substr(1, 2);
+	this.colorWord = code.substr(0, 1) == 'w' ? 'White' : 'Black';
+	this.nameWord = pieces[code.substr(1, 2)];
 }
 
 function checkForTaken(boardPosition) {
-    var pieceCount = {
-        wQ: 0
-        , wR: 0
-        , wB: 0
-        , wN: 0
-        , wP: 0
-        , wK: 0
-        , bQ: 0
-        , bR: 0
-        , bB: 0
-        , bN: 0
-        , bP: 0
-        , bK: 0
-    };
+	var pieceCount = {
+		wQ: 0,
+		wR: 0,
+		wB: 0,
+		wN: 0,
+		wP: 0,
+		wK: 0,
+		bQ: 0,
+		bR: 0,
+		bB: 0,
+		bN: 0,
+		bP: 0,
+		bK: 0
+	};
 
-    //Tally each piece
-    for (var property in boardPosition)
-        if (boardPosition.hasOwnProperty(property))
-            pieceCount[boardPosition[property]]++;
+	//Tally each piece
+	for (var property in boardPosition)
+		if (boardPosition.hasOwnProperty(property))
+			pieceCount[boardPosition[property]]++;
 
-    //Count differences to expected counts
-    pieceCount.wP = (-1) * (pieceCount.wP - 8);
-    pieceCount.bP = (-1) * (pieceCount.bP - 8);
-    pieceCount.bR = (-1) * (pieceCount.bR - 2);
-    pieceCount.bN = (-1) * (pieceCount.bN - 2);
-    pieceCount.bB = (-1) * (pieceCount.bB - 2);
-    pieceCount.bQ = (-1) * (pieceCount.bQ - 1);
-    pieceCount.bK = (-1) * (pieceCount.bK - 1);
-    pieceCount.wR = (-1) * (pieceCount.wR - 2);
-    pieceCount.wN = (-1) * (pieceCount.wN - 2);
-    pieceCount.wB = (-1) * (pieceCount.wB - 2);
-    pieceCount.wQ = (-1) * (pieceCount.wQ - 1);
-    pieceCount.wK = (-1) * (pieceCount.wK - 1);
+		//Count differences to expected counts
+	pieceCount.wP = (-1) * (pieceCount.wP - 8);
+	pieceCount.bP = (-1) * (pieceCount.bP - 8);
+	pieceCount.bR = (-1) * (pieceCount.bR - 2);
+	pieceCount.bN = (-1) * (pieceCount.bN - 2);
+	pieceCount.bB = (-1) * (pieceCount.bB - 2);
+	pieceCount.bQ = (-1) * (pieceCount.bQ - 1);
+	pieceCount.bK = (-1) * (pieceCount.bK - 1);
+	pieceCount.wR = (-1) * (pieceCount.wR - 2);
+	pieceCount.wN = (-1) * (pieceCount.wN - 2);
+	pieceCount.wB = (-1) * (pieceCount.wB - 2);
+	pieceCount.wQ = (-1) * (pieceCount.wQ - 1);
+	pieceCount.wK = (-1) * (pieceCount.wK - 1);
 
-    //Remove old pieces
-    var blackNode = document.getElementById(gui_blackCapturedId);
-    while (blackNode.firstChild)
-        blackNode.removeChild(blackNode.firstChild);
-    var whiteNode = document.getElementById(gui_whiteCapturedId);
-    while (whiteNode.firstChild)
-        whiteNode.removeChild(whiteNode.firstChild);
-    whiteNode.innerHTML = 'CAPTURED WHITE PIECES';
-    blackNode.innerHTML = 'CAPTURED BLACK PIECES';
-    
-    var p =  pieceCount;
-    var blackScore = p.wP + (p.wB * 3) + (p.wK * 3) + (p.wR * 4) +(p.wQ) + (p.wK * 0);
-    var whiteScore = p.bP + (p.bB * 3) + (p.bK * 3) + (p.bR * 4) +(p.bQ) + (p.bK * 0);
-    var divBlackScore = document.getElementById(gui_scoreBlackId);
-    var divWhiteScore = document.getElementById(gui_scoreWhiteId);
-    console.log(blackScore);
-    console.log(whiteScore);
-    divBlackScore.InnerHTML = "Black Score: " + toString(blackScore);
-    divWhiteScore.InnerHTML = "White Score: " + toString(whiteScore);
-    
-    
-    for (var property in pieceCount)
-        if (pieceCount.hasOwnProperty(property)) {
-            var isWhite = property.substr(0, 1) == 'w';
-            var div = isWhite ? document.getElementById(gui_whiteCapturedId) : document.getElementById(gui_blackCapturedId);
-            drawImg("img/chesspieces/wikipedia/" + property + ".png", div, pieceCount[property]);
-        }
+	//Remove old pieces
+	var blackNode = document.getElementById(gui_blackCapturedId);
+	while (blackNode.firstChild)
+		blackNode.removeChild(blackNode.firstChild);
+	var whiteNode = document.getElementById(gui_whiteCapturedId);
+	while (whiteNode.firstChild)
+		whiteNode.removeChild(whiteNode.firstChild);
+	whiteNode.innerHTML = 'CAPTURED WHITE PIECES';
+	blackNode.innerHTML = 'CAPTURED BLACK PIECES';
+
+	var p = pieceCount;
+	var blackScore = p.wP + (p.wB * 3) + (p.wK * 3) + (p.wR * 4) + (p.wQ) + (p.wK * 0);
+	var whiteScore = p.bP + (p.bB * 3) + (p.bK * 3) + (p.bR * 4) + (p.bQ) + (p.bK * 0);
+	var divBlackScore = document.getElementById(gui_scoreBlackId);
+	var divWhiteScore = document.getElementById(gui_scoreWhiteId);
+	console.log(blackScore);
+	console.log(whiteScore);
+	divBlackScore.InnerHTML = "Black Score: " + toString(blackScore);
+	divWhiteScore.InnerHTML = "White Score: " + toString(whiteScore);
+
+
+	for (var property in pieceCount)
+		if (pieceCount.hasOwnProperty(property)) {
+			var isWhite = property.substr(0, 1) == 'w';
+			var div = isWhite ? document.getElementById(gui_whiteCapturedId) : document.getElementById(gui_blackCapturedId);
+			drawImg("img/chesspieces/wikipedia/" + property + ".png", div, pieceCount[property]);
+		}
 }
 
 
 function drawImg(src, container, count) {
-    for (var i = 0; i < count; i++) {
-        img = document.createElement("img");
-        img.src = src;
-        img.style.height = gui_capturedPieceSize;
-        img.style.width = gui_capturedPieceSize;
-        container.appendChild(img);
-    }
+	for (var i = 0; i < count; i++) {
+		img = document.createElement("img");
+		img.src = src;
+		img.style.height = gui_capturedPieceSize;
+		img.style.width = gui_capturedPieceSize;
+		container.appendChild(img);
+	}
 }
 
 
 function updateDebugLog() {
-    var status = ''
-        , check = ''
-        , checkmate = '';
+	var status = '',
+		check = '',
+		checkmate = '';
 
-    var moveColor = (game.turn() === 'b' ? "Black" : "White");
+	var moveColor = (game.turn() === 'b' ? "Black" : "White");
 
-    // checkmate?
-    if (game.in_checkmate() === true) {
-        checkmate = moveColor;
-    } else if (game.in_draw() === true) {
-        status = 'DRAW';
-    } else {
-        status = moveColor;
-        if (game.in_check() === true) {
-            check = moveColor;
-        }
-    }
+	// checkmate?
+	if (game.in_checkmate() === true) {
+		checkmate = moveColor;
+	} else if (game.in_draw() === true) {
+		status = 'DRAW';
+	} else {
+		status = moveColor;
+		if (game.in_check() === true) {
+			check = moveColor;
+		}
+	}
 
-    $('#status').html("FEN: " + game.fen() + "<br><br>");
-    $('#fen').html("TURN: " + status);
-    $('#check').html("COLOUR IN CHECK: " + check);
-    $('#checkmate').html("COLOUR IN CHECKMATE: " + checkmate);
+	$('#status').html("FEN: " + game.fen() + "<br><br>");
+	$('#fen').html("TURN: " + status);
+	$('#check').html("COLOUR IN CHECK: " + check);
+	$('#checkmate').html("COLOUR IN CHECKMATE: " + checkmate);
 }
 
 function SetTheme(theme) {
-    addCSS(".square-55d63", cb_shapes[cb_currentTheme.boardShape]);
+	addCSS(".square-55d63", cb_shapes[cb_currentTheme.boardShape]);
 
-    if (cb_shapes[cb_currentTheme.boardShape] == cb_shapes["Diamond"])
-        addCSS(".square-55d63 img", cb_shapes["DiamondIMGFix"]);
+	if (cb_shapes[cb_currentTheme.boardShape] == cb_shapes["Diamond"])
+		addCSS(".square-55d63 img", cb_shapes["DiamondIMGFix"]);
 
-    alterCSS('.white-1e1d7', theme.whiteSquare, theme.whiteSquareText);
-    alterCSS('.black-3c85d', theme.blackSquare, theme.blackSquareText);
+	alterCSS('.white-1e1d7', theme.whiteSquare, theme.whiteSquareText);
+	alterCSS('.black-3c85d', theme.blackSquare, theme.blackSquareText);
 }
 
 function alterCSS(className, backgroundColour, textColour) {
 
-    //Create CSS var
-    var classCSS = "background-color: " + backgroundColour + ";" + " color:" + textColour + ";"
+	//Create CSS var
+	var classCSS = "background-color: " + backgroundColour + ";" + " color:" + textColour + ";"
 
-    //Remove Inline Style/Attr
-    if ($(className).removeProp) {
-        $(className).removeProp('background-color');
-    } else {
-        $(className).removeAttr('background-color');
-    }
+	//Remove Inline Style/Attr
+	if ($(className).removeProp) {
+		$(className).removeProp('background-color');
+	} else {
+		$(className).removeAttr('background-color');
+	}
 
-    //Create a hidden div for storing CSS information (add to eof)
-    var tempCSSContainer = $('#temp-css-store-c34jw2-f32r12');
-    if (tempCSSContainer.length == 0) {
-        var tempCSSContainer = $('<div id="temp-css-store-c34jw2-f32r12"></div>');
-        tempCSSContainer.hide();
-        tempCSSContainer.appendTo($('body'));
-    }
+	//Create a hidden div for storing CSS information (add to eof)
+	var tempCSSContainer = $('#temp-css-store-c34jw2-f32r12');
+	if (tempCSSContainer.length == 0) {
+		var tempCSSContainer = $('<div id="temp-css-store-c34jw2-f32r12"></div>');
+		tempCSSContainer.hide();
+		tempCSSContainer.appendTo($('body'));
+	}
 
-    //Create a Div for each class element found with className and append to HTML
-    classContainer = tempCSSContainer.find('div[data-class="' + className + '"]');
-    if (classContainer.length == 0) {
-        classContainer = $('<div data-class="' + className + '"></div>');
-        classContainer.appendTo(tempCSSContainer);
-    }
+	//Create a Div for each class element found with className and append to HTML
+	classContainer = tempCSSContainer.find('div[data-class="' + className + '"]');
+	if (classContainer.length == 0) {
+		classContainer = $('<div data-class="' + className + '"></div>');
+		classContainer.appendTo(tempCSSContainer);
+	}
 
-    //Add the additional style to the parent Div and Style it with overridden CSS
-    classContainer.html('<style>' + className + ' {' + classCSS + '}</style>');
+	//Add the additional style to the parent Div and Style it with overridden CSS
+	classContainer.html('<style>' + className + ' {' + classCSS + '}</style>');
 }
 
 function addCSS(className, classCSS) {
 
-    //Remove Inline Style/Attr
-    if ($(className).removeProp) {
-        $(className).removeProp('background-color');
-    } else {
-        $(className).removeAttr('background-color');
-    }
+	//Remove Inline Style/Attr
+	if ($(className).removeProp) {
+		$(className).removeProp('background-color');
+	} else {
+		$(className).removeAttr('background-color');
+	}
 
-    //Create a hidden div for storing CSS information (add to eof)
-    var tempCSSContainer = $('#temp-css-store-c34jw2-f32r12');
-    if (tempCSSContainer.length == 0) {
-        var tempCSSContainer = $('<div id="temp-css-store-c34jw2-f32r12"></div>');
-        tempCSSContainer.hide();
-        tempCSSContainer.appendTo($('body'));
-    }
+	//Create a hidden div for storing CSS information (add to eof)
+	var tempCSSContainer = $('#temp-css-store-c34jw2-f32r12');
+	if (tempCSSContainer.length == 0) {
+		var tempCSSContainer = $('<div id="temp-css-store-c34jw2-f32r12"></div>');
+		tempCSSContainer.hide();
+		tempCSSContainer.appendTo($('body'));
+	}
 
-    //Create a Div for each class element found with className and append to HTML
-    classContainer = tempCSSContainer.find('div[data-class="' + className + '"]');
-    if (classContainer.length == 0) {
-        classContainer = $('<div data-class="' + className + '"></div>');
-        classContainer.appendTo(tempCSSContainer);
-    }
+	//Create a Div for each class element found with className and append to HTML
+	classContainer = tempCSSContainer.find('div[data-class="' + className + '"]');
+	if (classContainer.length == 0) {
+		classContainer = $('<div data-class="' + className + '"></div>');
+		classContainer.appendTo(tempCSSContainer);
+	}
 
-    //Add the additional style to the parent Div and Style it with overridden CSS
-    classContainer.html('<style>' + className + ' {' + classCSS + '}</style>');
+	//Add the additional style to the parent Div and Style it with overridden CSS
+	classContainer.html('<style>' + className + ' {' + classCSS + '}</style>');
 }
--- js/engine.js
+++ js/engine.js
@@ -1,6 +1,5 @@
-
 /* Stockfish Globals */
-Engine = typeof STOCKFISH === "function" ? STOCKFISH() : new Worker('js/opensource/stockfish.js');
+stockfishEngine = typeof STOCKFISH === "function" ? STOCKFISH() : new Worker('js/opensource/stockfish.js');
 
 queryQueue = [];
 /*                   */
@@ -9,55 +8,55 @@ function Init_Stockfish() {
 
 }
 
-//Query the engine from the tutor or opponent
-function AskEngine(source, side, fen) {
-    var query = {
-          source: source
-        , side: side
-        , fen: fen
-        , move: 'QUERY UNRESOLVED'
-    }
-    queryQueue.push(query);
-    //If first item, send the query straight away
-    if (queryQueue.length === 1)
-        QueryEngine(query["fen"], sf_searchDepth);
-    else //Stop the engine search
-    {
-        Engine.postMessage("stop");
-        queryQueue[0]['move'] = 'STOP';
-    }
+//Query the stockfishEngine from the tutor or opponent
+function AskstockfishEngine(source, side, fen) {
+	var query = {
+		source: source,
+		side: side,
+		fen: fen,
+		move: 'QUERY UNRESOLVED'
+	}
+	queryQueue.push(query);
+	//If first item, send the query straight away
+	if (queryQueue.length === 1)
+		QuerystockfishEngine(query["fen"], sf_searchDepth);
+	else //Stop the stockfishEngine search
+	{
+		stockfishEngine.postMessage("stop");
+		queryQueue[0]['move'] = 'STOP';
+	}
 }
 
 //Message recieved
-Engine.onmessage = function (event) {
-        //When the engine outputs 'bestmove' the search has finished
-        if ((String(event.data).substring(0, 8) == 'bestmove') && queryQueue[0]) {
-            //If no interruption, assign the move info
-            if (queryQueue[0].move !== 'STOP')
-                queryQueue[0].move = String(event.data).substring(9, 13);
-            //Remove the head of the queue
-            ReturnQuery(queryQueue.shift());
-            //If the queue still has queries, go to next query
-            if (queryQueue.length > 0) {
-                QueryEngine(queryQueue[0]["fen"], sf_searchDepth);
-            }
-        }
-    }
-    //Format string for engine message
-function QueryEngine(fen, depth) {
-    Engine.postMessage("position fen " + fen);
-    Engine.postMessage("go depth " + depth);
+stockfishEngine.onmessage = function (event) {
+		//When the stockfishEngine outputs 'bestmove' the search has finished
+		if ((String(event.data).substring(0, 8) == 'bestmove') && queryQueue[0]) {
+			//If no interruption, assign the move info
+			if (queryQueue[0].move !== 'STOP')
+				queryQueue[0].move = String(event.data).substring(9, 13);
+			//Remove the head of the queue
+			ReturnQuery(queryQueue.shift());
+			//If the queue still has queries, go to next query
+			if (queryQueue.length > 0) {
+				QuerystockfishEngine(queryQueue[0]["fen"], sf_searchDepth);
+			}
+		}
+	}
+	//Format string for stockfishEngine message
+function QuerystockfishEngine(fen, depth) {
+	stockfishEngine.postMessage("position fen " + fen);
+	stockfishEngine.postMessage("go depth " + depth);
 }
 
 function ReturnQuery(query) {
-    $("#suggestedMove").html("SUGGESTED MOVE FOR " + (query['side'] == "w" ? "White" : "Black") + ": " + query['move']);
-    
-    console.log(" * Turn: " + (turnCount === 0 ? "1" : Math.floor(turnCount / 2)) + " Side: " + query['side'] + " Move: " + query['move']);
+	$("#suggestedMove").html("SUGGESTED MOVE FOR " + (query['side'] == "w" ? "White" : "Black") + ": " + query['move']);
 
-    if (game.game_over()) return;
+	console.log(" * Turn: " + (turnCount === 0 ? "1" : Math.floor(turnCount / 2)) + " Side: " + query['side'] + " Move: " + query['move']);
 
-    //Make opponent moves
-    if (cb_autoPlay || (game_pve && (game_playerSide != query.side))) {
-        cb_autoPlayMove = setTimeout(MovePiece, cb_autoPlayDelay, query.move.substr(0, 2), query.move.substr(2, 4));
-    }
+	if (game.game_over()) return;
+
+	//Make opponent moves
+	if (cb_autoPlay || (game_pve && (game_playerSide != query.side))) {
+		cb_autoPlayMove = setTimeout(MovePiece, cb_autoPlayDelay, query.move.substr(0, 2), query.move.substr(2, 4));
+	}
 }
