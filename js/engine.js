c8ed0e285a43555ba3815270387bfd86a3b2041a
Raymond Kirk <ray.tunstill@live.co.uk>
2016-06-12 11:30:31 +0100

    Ordering

--- js/engine.js
+++ js/engine.js
@@ -1,12 +1,6 @@
 /* Stockfish Globals */
 var stockfishEngine = typeof STOCKFISH === "function" ? STOCKFISH() : new Worker('js/opensource/stockfish.js');
 
-//Format string for stockfishEngine message
-function AskEngine(fen, depth) {
-	stockfishEngine.postMessage("position fen " + fen);
-	stockfishEngine.postMessage("go depth " + depth);
-}
-
 stockfishEngine.onmessage = function (event) {
 	if (event.data.toString().substr(0, 8) !== 'bestmove')
 		return;
@@ -21,4 +15,10 @@ stockfishEngine.onmessage = function (event) {
 	if (!game.game_over() && cb_autoPlay || (game_pve && (game_playerSide != side))) {
 		cb_autoPlayMove = setTimeout(MovePiece, cb_autoPlayDelay, move.substr(0, 2), move.substr(2, 4));
 	}
+}
+
+//Format string for stockfishEngine message
+function AskEngine(fen, depth) {
+	stockfishEngine.postMessage("position fen " + fen);
+	stockfishEngine.postMessage("go depth " + depth);
 }
