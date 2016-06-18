ddcf213cda481c1dc227e8be6c9fe40f251ab4fa
Raymond Kirk <ray.tunstill@live.co.uk>
2016-06-18 17:45:28 +0100

    Changed port

--- app.js
+++ app.js
@@ -77,6 +77,6 @@ io.on('connection', function (socket) {
 	socket.join(socket.id);
 });
 
-http.listen(3000, function () {
+http.listen(process.env.PORT || 3000, function () {
 	console.log('listening on *:3000');
 });
